57b189b298f6b7be14ec8d627e61205bae3857ec
Brandon Turner <brandon.turner@runzero.com>
2024-12-18 22:05:03 -0600

    Fix PDF generator job

--- pdfgen/makepdf.js
+++ pdfgen/makepdf.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: true, args: ["--no-sandbox", "--disable-setuid-sandbox"]});
     const page = await browser.newPage();
     await page.goto('file://' + __dirname + '/html/index.html', {
         waitUntil: 'networkidle0',
